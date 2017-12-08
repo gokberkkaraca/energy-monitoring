a481df8ca7b3dcbb36cda44cd3bf04dfcac83f6e
Gökberk Karaca <gskaraca1905@gmail.com>
2017-12-08 20:01:22 +0100

    Add Main Activity and Profile Activity

--- mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/MainActivity.java
+++ mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/MainActivity.java
@@ -1,13 +1,43 @@
 package ee490g.epfl.ch.dwarfsleepy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+
+import ee490g.epfl.ch.dwarfsleepy.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setButtonClickListener();
+    }
+
+    private void initViews() {
+        profileButton = findViewById(R.id.profileButton);
+    }
+
+    private void setButtonClickListener() {
+        profileButton.setOnClickListener(new ButtonClickListener());
+    }
+
+    class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.profileButton:
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                default:
+                    break;
+            }
+        }
     }
 }
---
+++ mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/ProfileActivity.java
@@ -0,0 +1,59 @@
+package ee490g.epfl.ch.dwarfsleepy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuthManager authManager = new FirebaseAuthManager();
+
+    private Button logoutButton;
+
+    private TextView userTextView;
+    private TextView emailTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        initViews();
+        setButtonClickListener();
+    }
+
+    private void initViews() {
+        logoutButton = findViewById(R.id.logoutButton);
+
+        userTextView = findViewById(R.id.userTextView);
+        emailTextView = findViewById(R.id.emailTextView);
+
+        userTextView.setText(authManager.getCurrentUser().getDisplayName());
+        emailTextView.setText(authManager.getCurrentUser().getEmail());
+    }
+
+    private void setButtonClickListener() {
+        logoutButton.setOnClickListener(new ButtonClickListener());
+    }
+
+    private void performLogout() {
+        authManager.signOut();
+    }
+
+
+    class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.logoutButton:
+                    performLogout();
+                    startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                default:
+                    break;
+            }
+        }
+    }
+}
