a514de2ed1a27145a5019adcd7660eba2c566884
Gökberk Karaca <gskaraca1905@gmail.com>
2017-12-09 20:17:35 +0100

    Display heart rate on watch interface

--- wear/src/main/java/ee490g/epfl/ch/dwarfsleepy/MainActivity.java
+++ wear/src/main/java/ee490g/epfl/ch/dwarfsleepy/MainActivity.java
@@ -1,19 +1,52 @@
 package ee490g.epfl.ch.dwarfsleepy;
 
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+public class MainActivity extends WearableActivity implements SensorEventListener{
+
+    TextView textViewHeartRate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView mTextView = (TextView) findViewById(R.id.text);
-
         // Enables Always-on
         setAmbientEnabled();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && checkSelfPermission("android.permission.BODY_SENSORS") ==
+                PackageManager.PERMISSION_DENIED) {
+            requestPermissions(new String[]
+                    {"android.permission.BODY_SENSORS"},0);
+        }
+
+        SensorManager sensorManager = (SensorManager) getSystemService(MainActivity.SENSOR_SERVICE);
+        assert sensorManager != null;
+        Sensor heartRateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+        sensorManager.registerListener(this,heartRateSensor,SensorManager.SENSOR_DELAY_UI);
+
+        textViewHeartRate = findViewById(R.id.textViewHeartRate);
+
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d("HeartRate", "onAccuracyChanged - accuracy: " + accuracy);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (textViewHeartRate != null)
+            textViewHeartRate.setText(String.valueOf(event.values[0]));
+    }
+
 }
