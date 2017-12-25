d1fb73b93136236da4453685896aedd0fdee2c23
Gökberk Karaca <gskaraca1905@gmail.com>
2017-12-25 19:48:38 +0100

    Plot the heart rates, some bugs with user interface

--- mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/DayMonitoringActivity.java
+++ mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/DayMonitoringActivity.java
@@ -5,6 +5,18 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.util.ArrayList;
+import java.util.List;
+
 import ee490g.epfl.ch.dwarfsleepy.models.HeartRateData;
 import ee490g.epfl.ch.dwarfsleepy.models.User;
 import ee490g.epfl.ch.dwarfsleepy.utils.NavigationHandler;
@@ -17,6 +29,9 @@ public class DayMonitoringActivity extends AppCompatActivity {
     private TextView heartRateTextView;
     private TextView caloriesBurntTextView;
 
+    private XYPlot heartRatePlot;
+    private XYPlot accelerometerPlot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +43,50 @@ public class DayMonitoringActivity extends AppCompatActivity {
 
         initializeViews();
         setHeartRateView();
+        plotHeartRates();
+        plotAccelerometer();
+    }
+
+    private void plotAccelerometer() {
+
+    }
+
+    private void plotHeartRates() {
+        final List<Integer> heartRates = new ArrayList<>();
+        final List<Number> timeStamps = new ArrayList<>();
+
+        if (averagedHeartRateDataList.size() < 600)
+            for (int i = 0; i < averagedHeartRateDataList.size(); i++) {
+                heartRates.add(averagedHeartRateDataList.get(i).getValue().intValue());
+                timeStamps.add(i);
+            }
+        else {
+            for (int i = 0; i < 600; i++) {
+                heartRates.add(averagedHeartRateDataList.get(i).getValue().intValue());
+            }
+        }
+
+
+        XYSeries heartRateSeries =
+                new SimpleXYSeries(heartRates, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Heart Rates");
+
+        LineAndPointFormatter heartRateSeriesFormat =
+                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
+
+        heartRatePlot.addSeries(heartRateSeries, heartRateSeriesFormat);
+
+        heartRatePlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                int i = Math.round(((Number) obj).floatValue());
+                return toAppendTo.append(timeStamps.toArray()[i]);
+            }
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+
     }
 
     private void setHeartRateView() {
@@ -48,6 +107,8 @@ public class DayMonitoringActivity extends AppCompatActivity {
     private void initializeViews() {
         heartRateTextView = findViewById(R.id.heartRateTextView);
         caloriesBurntTextView = findViewById(R.id.caloriesBurntTextView);
+        heartRatePlot = findViewById(R.id.heartRatePlot);
+        accelerometerPlot = findViewById(R.id.accelerometerPlot);
     }
 
     @Override
