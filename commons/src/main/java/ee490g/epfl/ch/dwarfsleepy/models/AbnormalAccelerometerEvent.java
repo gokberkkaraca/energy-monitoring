be8e1bd02fcdc8be6d8ba0aa0adaf2bb44ae1638
Gökberk Karaca <gskaraca1905@gmail.com>
2017-12-27 17:33:12 +0100

    Implement AbnormalAccelerometerEvent class

--- commons/src/main/java/ee490g/epfl/ch/dwarfsleepy/models/AbnormalAccelerometerEvent.java
+++ commons/src/main/java/ee490g/epfl/ch/dwarfsleepy/models/AbnormalAccelerometerEvent.java
@@ -6,19 +6,101 @@ import java.util.Date;
 
 public class AbnormalAccelerometerEvent {
 
-    // TODO Add more fields
-    private Date date;
+    private Float xAxisValue;
+    private Float yAxisValue;
+    private Float zAxisValue;
+    private Float accelerometerValue;
 
-    public AbnormalAccelerometerEvent(Date date) {
-        this.date = date;
+    private Date beginTime;
+    private Date endTime;
+    private int durationMilliseconds;
+    private int durationSeconds;
+    private int durationMinutes;
+    private int durationHours;
+
+    public AbnormalAccelerometerEvent(Float xAxisValue, Float yAxisValue, Float zAxisValue, Date beginTime, Date endTime) {
+        this.xAxisValue = xAxisValue;
+        this.yAxisValue = yAxisValue;
+        this.zAxisValue = zAxisValue;
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+
+        this.accelerometerValue = (float) Math.sqrt(Math.pow(xAxisValue, 2) + Math.pow(yAxisValue, 2) + Math.pow(zAxisValue, 2));
+
+        long duration = endTime.getTime() - beginTime.getTime();
+        this.durationMilliseconds = (int) (duration) % 1000;
+        this.durationSeconds = (int) (duration / 1000) % 60 ;
+        this.durationMinutes = (int) ((duration / (1000*60)) % 60);
+        this.durationHours = (int) ((duration / (1000*60*60)) % 24);
+    }
+
+    public AbnormalAccelerometerEvent() {
+
     }
 
     public AbnormalAccelerometerEvent(DataMap dataMap) {
-        this.date = new Date(dataMap.getLong("date"));
+        this.xAxisValue = dataMap.getFloat("xAxisValue");
+        this.yAxisValue = dataMap.getFloat("yAxisValue");
+        this.zAxisValue = dataMap.getFloat("xAxisValue");
+        this.accelerometerValue = (float) Math.sqrt(Math.pow(xAxisValue, 2) + Math.pow(yAxisValue, 2) + Math.pow(zAxisValue, 2));
+
+        this.beginTime = new Date(dataMap.getLong("beginTime"));
+        this.endTime = new Date(dataMap.getLong("endTime"));
+
+        long duration = dataMap.getLong("duration");
+        this.durationMilliseconds = (int) (duration) % 1000;
+        this.durationSeconds = (int) (duration / 1000) % 60 ;
+        this.durationMinutes = (int) ((duration / (1000*60)) % 60);
+        this.durationHours = (int) ((duration / (1000*60*60)) % 24);
     }
 
     public DataMap putToDataMap(DataMap dataMap) {
-        dataMap.putLong("date", date.getTime());
+        dataMap.putFloat("xAxisValue", xAxisValue);
+        dataMap.putFloat("yAxisValue", yAxisValue);
+        dataMap.putFloat("zAxisValue", zAxisValue);
+        dataMap.putLong("beginTime", beginTime.getTime());
+        dataMap.putLong("endTime", endTime.getTime());
+        dataMap.putLong("duration", endTime.getTime() - beginTime.getTime());
         return dataMap;
+    }
+
+    public Float getxAxisValue() {
+        return xAxisValue;
+    }
+
+    public Float getyAxisValue() {
+        return yAxisValue;
+    }
+
+    public Float getzAxisValue() {
+        return zAxisValue;
+    }
+
+    public Float getAccelerometerValue() {
+        return accelerometerValue;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public int getDurationMilliseconds() {
+        return durationMilliseconds;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public int getDurationHours() {
+        return durationHours;
     }
 }
--- wear/src/main/java/ee490g/epfl/ch/dwarfsleepy/data/DataHolder.java
+++ wear/src/main/java/ee490g/epfl/ch/dwarfsleepy/data/DataHolder.java
@@ -2,6 +2,7 @@ package ee490g.epfl.ch.dwarfsleepy.data;
 
 import java.util.ArrayList;
 
+import ee490g.epfl.ch.dwarfsleepy.models.AbnormalAccelerometerEvent;
 import ee490g.epfl.ch.dwarfsleepy.models.AbnormalHeartRateEvent;
 import ee490g.epfl.ch.dwarfsleepy.models.AccelerometerData;
 import ee490g.epfl.ch.dwarfsleepy.models.HeartRateData;
@@ -10,5 +11,7 @@ public class DataHolder {
 
     public static ArrayList<HeartRateData> averagedHeartRateDataList = new ArrayList<>();
     public static ArrayList<AbnormalHeartRateEvent> abnormalHeartRateEvents = new ArrayList<>();
+
     public static ArrayList<AccelerometerData> averagedAccelerometerDataList = new ArrayList<>();
+    public static ArrayList<AbnormalAccelerometerEvent> abnormalAccelerometerEvents = new ArrayList<>();
 }
