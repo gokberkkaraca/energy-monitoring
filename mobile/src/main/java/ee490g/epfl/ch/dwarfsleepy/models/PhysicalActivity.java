b98306d18e7bb96240b71839b7bd9e3bb41ca162
Gökberk Karaca <gskaraca1905@gmail.com>
2017-12-29 13:43:04 +0100

    Add new class for physical activities

---
+++ mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/models/PhysicalActivity.java
@@ -0,0 +1,36 @@
+package ee490g.epfl.ch.dwarfsleepy.models;
+
+
+import java.util.Date;
+
+public class PhysicalActivity {
+
+    public enum ActivityType {
+        BIKING,
+        WALKING,
+        RUNNING,
+        OTHER
+    }
+
+    private ActivityType activityType;
+    private Date beginTime;
+    private Date endTime;
+
+    public PhysicalActivity(ActivityType activityType, Date beginTime, Date endTime) {
+        this.activityType = activityType;
+        this.beginTime = beginTime;
+        this.endTime = endTime;
+    }
+
+    public ActivityType getActivityType() {
+        return activityType;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+}
