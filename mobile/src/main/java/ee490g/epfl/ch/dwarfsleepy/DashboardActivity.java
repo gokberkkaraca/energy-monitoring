bba755595a7a95b335f276fba14572bc6b4dc1c1
Osman Berk <osman.satir@epfl.ch>
2018-01-04 00:23:32 +0100

    Getting Google Fit Data between 9-23.59

--- mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/DashboardActivity.java
+++ mobile/src/main/java/ee490g/epfl/ch/dwarfsleepy/DashboardActivity.java
@@ -318,13 +318,14 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         Calendar cal = Calendar.getInstance();
         Date now = new Date();
         cal.setTime(now);
-        cal.set(Calendar.HOUR_OF_DAY, 21);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
         cal.set(Calendar.MILLISECOND, 0);
 
         long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.HOUR_OF_DAY, -12);
+        cal.add(Calendar.HOUR_OF_DAY, -15);
+        cal.add(Calendar.SECOND,1);
         long startTime = cal.getTimeInMillis();
 
         DateFormat dateFormat = getDateInstance();
